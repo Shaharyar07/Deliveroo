3e4feafc776affa8b1ef449620b206ad479d9366
Shaharyar07 <shaharyar.tariq248@gmail.com>
2022-08-11 03:09:38 +0500

    category card added

--- components/Categories.js
+++ components/Categories.js
@@ -1,11 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import CategoryCard from "./CategoryCard";
 
 const Categories = () => {
   return (
-    <View>
-      <Text>Categories</Text>
-    </View>
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+      }}
+    >
+      {/* CategoryCard */}
+      <CategoryCard imgUrl='https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450' title='Testing' />
+      <CategoryCard imgUrl='https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450' title='Testing' />
+      <CategoryCard imgUrl='https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450' title='Testing' />
+    </ScrollView>
   );
 };
 
