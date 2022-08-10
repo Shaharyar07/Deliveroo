b8c2f425872624d4b5cfcbb4513936438b7960d8
Shaharyar07 <shaharyar.tariq248@gmail.com>
2022-08-11 02:15:50 +0500

    .

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,20 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
+import {
+  AdjustmentsIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  UserIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -12,27 +25,43 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView style={{ marginVertical: 35 }}>
-      <Text className='text-red-500'>
-        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
-          <Image
-            source={{
-              uri: "https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450",
-            }}
-            className='w-7 h-7 bg-gray-300 p-5 rounded-full'
-          />
-          <View className='flex-1'>
-            <Text className='font-bold text-gray-400 text-xs'>
-              Deliver Now!
-            </Text>
-            <Text className='font-bold text-xl'>
-              Current location
-              <ChevronDownIcon size={20} color='#00CCBB' />
-            </Text>
-          </View>
-          <UserIcon size={35} color='#00CCBB' />
+    <SafeAreaView
+      style={{ marginVertical: 40, backgroundColor: "white", paddingTop: 10 }}
+    >
+      {/* Header */}
+      <View className='relative flex-row pb-3 items-center mx-4 space-x-2'>
+        <Image
+          source={{
+            uri: "https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450",
+          }}
+          className='w-7 h-7 bg-gray-300 p-5 rounded-full'
+        />
+        <View className='flex-1'>
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+          <Text className='font-bold text-xl'>
+            Current location
+            <ChevronDownIcon size={20} color='#00CCBB' />
+          </Text>
         </View>
-      </Text>
+
+        <UserIcon size={35} color='#00CCBB' />
+      </View>
+      {/* Search */}
+      <View className='flex-row items-center space-x-2 pb-2 mx-4'>
+        <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
+          <SearchIcon color='gray' size={20} />
+          <TextInput
+            placeholder='Restaurants and cuisines'
+            keyboardType='default'
+          />
+        </View>
+        <AdjustmentsIcon color='#00CCBB' />
+      </View>
+
+      <ScrollView>
+        {/* Categories */}
+        <Categories />
+      </ScrollView>
     </SafeAreaView>
   );
 };
