a5d3cd97b6fcef37c4163c2b35d21ac058a9762d
Shaharyar07 <shaharyar.tariq248@gmail.com>
2022-08-14 04:05:22 +0500

    categories added from sanity

--- components/Categories.js
+++ components/Categories.js
@@ -1,8 +1,20 @@
 import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import CategoryCard from "./CategoryCard";
+import client, { urlFor } from "../sanity";
 
 const Categories = () => {
+  const [categories, setCategories] = React.useState([]);
+  React.useEffect(() => {
+    client
+      .fetch(
+        `*[_type == "category"] 
+    `
+      )
+      .then((data) => {
+        setCategories(data);
+      });
+  }, []);
   return (
     <ScrollView
       horizontal
@@ -13,12 +25,13 @@ const Categories = () => {
       }}
     >
       {/* CategoryCard */}
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='Testing' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='Testing' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='Testing' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='Testing' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='Testing' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='Testing' />
+      {categories.map((category) => (
+        <CategoryCard
+          key={category._id}
+          imgUrl={urlFor(category.image).url()}
+          title={category.name}
+        />
+      ))}
     </ScrollView>
   );
 };
